a7fc73f655d89387f0d32c71c0974956bd72d40f
Carlos Correa <carloscorrea137@gmail.com>
2016-12-12 18:58:14 -0800

    add colorbar to the right, per http://stackoverflow.com/a/13784887

--- tf-nonlinearity/2d_result_plot.py
+++ tf-nonlinearity/2d_result_plot.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import pickle
 import numpy.ma as ma
+import numpy as np
 
 
 if __name__ == '__main__':
-    plt.figure(figsize=(8, 9))
+    fig = plt.figure(figsize=(10, 8))
+
+    data = []
+    all_errors = []
     for idx, f in enumerate([
         'c_0_tf_errors',
         'c_0.1_tf_errors',
@@ -12,25 +16,38 @@ if __name__ == '__main__':
     ]):
         with open(f, 'rb') as f:
             d = pickle.load(f)
-            Is = d['Is']
-            Ns = d['Ns']
-            C = d['C']
-            errors = d['errors']
+            d['errors'] = ma.masked_invalid(d['errors'])
+            data.append(d)
+            all_errors.append(d['errors'].flatten())
 
+    all_errors = np.concatenate(all_errors)
+    all_errors = all_errors[~np.isnan(all_errors)]
+    vmin = np.min(all_errors)
+    vmax = np.max(all_errors)
+
+    for idx, d in enumerate(data):
+        Is = d['Is']
+        Ns = d['Ns']
+        C = d['C']
+
+        errors = d['errors']
         I_step = Is[1] - Is[0]
         N_step = Ns[1] - Ns[0]
         ax = plt.subplot(2, 2, idx + 1)
-        errors = ma.masked_invalid(errors)
-        plt.pcolormesh(
+        im = plt.pcolormesh(
             Is + [Is[-1] + I_step],
             Ns + [Ns[-1] + N_step],
             errors,
+            vmin=vmin,
+            vmax=vmax,
             cmap='cool')
         plt.axis([0, Is[-1] + I_step, 0, Ns[-1] + N_step])
         plt.xlabel('I (every {}, from {} to {})'.format(I_step, Is[0], Is[-1]))
         plt.ylabel('N (every {}, from {} to {})'.format(N_step, Ns[0], Ns[-1]))
         plt.title('C={}'.format(C))
-    plt.subplot(2, 2, 4)
-    plt.colorbar(shrink=0.5, aspect=5)
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    fig.colorbar(im, cax=cbar_ax)
 
     plt.show()
