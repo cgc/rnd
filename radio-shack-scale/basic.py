2c7d6ab96e29805472cd5a8a87eec4473e6fe07c
Carlos Correa <carloscorrea137@gmail.com>
2019-12-24 18:22:49 -0600

    updates

--- radio-shack-scale/basic.py
+++ radio-shack-scale/basic.py
@@ -5,6 +5,14 @@ import tkinter as tk
 import signal
 
 units_to_grams = 1/2.674 # Guess based on seeing what other program does
+grams_to_ounces = 1/28.3495
+ounces_to_pounds = 1/16
+units = {
+    'Grams': (units_to_grams, 'g', 0),
+    'Kilograms': (units_to_grams/1000, 'kg', 3),
+    'Ounces': (units_to_grams*grams_to_ounces, 'oz', 1),
+    'Pounds': (units_to_grams*grams_to_ounces*ounces_to_pounds, 'lb', 2),
+}
 
 @contextmanager
 def usb_scale():
@@ -31,16 +39,15 @@ def usb_scale():
         if h:
             h.close()
 
-
 with usb_scale() as read:
 
     zero = read()
-    conv = units_to_grams
-    unit_string = 'g'
+    defval = 'Grams'
+    conv, unit_string, places = units[defval]
     shutdown = False
 
     def format_read():
-        return str(int((read() - zero) * conv))+unit_string
+        return ('{:.0'+str(places)+'f}{}').format((read() - zero) * conv, unit_string)
 
     def zero_fn():
         global zero
@@ -56,20 +63,30 @@ with usb_scale() as read:
       count()
 
     root = tk.Tk()
-    root.title('Radio Shack scale')
+    root.title('Radio Shack Scale')
     label = tk.Label(root, fg='black')
     label.pack()
     counter_label(label)
+
+    variable = tk.StringVar(root)
+    variable.set(defval)
+    w = tk.OptionMenu(root, variable, *units.keys())
+    w.pack()
+    def change_dropdown(*args):
+        global conv, unit_string, places
+        conv, unit_string, places = units[variable.get()]
+    variable.trace('w', change_dropdown)
+
     zero_btn = tk.Button(root, text='Zero', width=25, command=zero_fn)
     zero_btn.pack()
+
     button = tk.Button(root, text='Stop', width=25, command=root.destroy)
     button.pack()
 
+    # interrupt handler
     def sigint_handler(sig, frame):
         shutdown = True
-        print('hi2')
         root.destroy()
-        print('hi')
     signal.signal(signal.SIGINT, sigint_handler)
 
     root.mainloop()
